2942aa0c060156493f1e64b59caa51002896d558
Denis Basov <den4er@mail.ru>
2024-03-26 22:18:14 +0300

    Функции

--- 02 Управляющие конструкции/script.js
+++ 02 Управляющие конструкции/script.js
@@ -167,3 +167,133 @@ let greet = `<h2 class='head'>Hello, ${fName}</h2>`;
 console.log(greet);
 document.write(greet);
 */
+
+/**
+ * ДЗ
+ */
+// 5 Выведите в косоль только четные числа. Диапазон перебираемых чисел от 3 до 100.
+/*
+for (let i = 3; i <= 100; i++) {
+  // если число четное, выводим его в консоль
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+*/
+/*
+// && - логическое И
+// || - логическое ИЛИ
+// ! - логическое отрицание
+
+let numClock = 14;
+// 1 четверть от 0 до 15
+if(numClock > 0 && numClock <= 15 ){
+  console.log('1 четверть');
+}
+*/
+
+/*
+// 9* Напишите цикл, который считает сумму чисел от 1 до 10.
+let result = 0; // начальное значение для суммы
+for (let i = 1; i < 10; i++) {
+  //  console.log(i);
+  result += i; // накаждой итерации кладем текущее значение переменной i
+  //console.log(result);
+}
+console.log(result);
+*/
+
+/*
+// 9* Напишите цикл, который считает произведение чисел от 1 до 10.
+let result = 1; // начальное значение для суммы
+for (let i = 1; i < 10; i++) {
+  //  console.log(i);
+  result *= i; // накаждой итерации кладем текущее значение переменной i
+  //console.log(result);
+}
+console.log(result);
+*/
+/**
+ * конец ДЗ
+ */
+
+/*
+// прерываение цикла - break
+for (let i = 0; i < 10; i++) {
+  if (i === 5) {
+    break; // прерываем цикл
+  }
+  console.log(i);
+}
+*/
+
+/*
+// переход к следующей итерации цикла - continue
+// вывод четных чисел
+for (let i = 0; i <= 100; i++) {
+  // если число НЕчетное, переходим к след итерации
+  if (i % 2 === 1) {
+    continue; // к следующей итерации цикла
+  }
+  console.log(i);
+}
+*/
+
+// let students = ["Иван", "Анна", "Сергей", "Инна", "Павел"];
+// let numbers = [4, 5, 7, 3, 5, 7, 8];
+/*
+console.log(students);
+console.log(students[0]);
+console.log(students[2]);
+
+console.log(numbers[2]);
+*/
+//document.write(students);
+
+// document.write(students[0]);
+// document.write(students[1]);
+// document.write(students[2]);
+// console.log(students);
+// console.log(students.length); // получаем длину массива
+/*
+for (let i = 0; i < students.length; i++) {
+  //console.log(i);
+  console.log(students[i]);
+  document.write(`<h3>${students[i]}</h3>`);
+}
+*/
+/*
+let fName = "Иван" + " ";
+console.log(fName);
+*/
+
+/**
+ * for ... of
+ */
+
+/*
+let students = ["Иван", "Анна", "Сергей", "Инна", "Павел"];
+let numbers = [4, 5, 7, 3, 5, 7, 8];
+*/
+
+/*
+for (let student of students) {
+  console.log(student);
+  document.write(`<h2>${student}</h2>`);
+}
+
+for (let number of numbers) {
+  console.log(number);
+  document.write(`<h2>${number}</h2>`);
+}
+*/
+
+// получить из массива строк одну строку
+/*
+let studentsStr = "";
+for (let student of students) {
+  studentsStr += student + " ";
+}
+console.log(studentsStr);
+document.write(`<h2>${studentsStr}</h2>`);
+*/
---
+++ 03 Функции/script.js
@@ -0,0 +1,191 @@
+// 1 объявление функции
+// function sayHi() {
+//   console.log("Hi");
+// }
+
+// 2 вызов функции
+// sayHi();
+// sayHi();
+// sayHi();
+// sayHi();
+
+/*
+let userName = "Иван";
+console.log(userName);
+console.log(userName.length); // получаем длину строки
+console.log(userName[0]);
+*/
+
+/*
+function getSum() {
+  console.log(2 + 2);
+}
+getSum();
+getSum();
+getSum();
+getSum();
+*/
+
+// num1, num2 - параметры функции
+/*
+function getSum(num1, num2) {
+  console.log(num1 + num2);
+}
+
+getSum(2, 2);
+getSum(5, 5);
+getSum(43, 78);
+*/
+
+/*
+let age = "hello";
+console.log(typeof age);
+*/
+
+/*
+// функция для получения суммы элементов массива
+function getArraySum(arrayNum) {
+  let sum = 0;
+  for (let num of arrayNum) {
+    sum += num;
+  }
+  //console.log(typeof sum);
+  if (typeof sum === "number") {
+    console.log(sum);
+  } else {
+    console.log("Внимание! В массиве есть элемент не числового типа'!!");
+  }
+}
+
+let nums = [3, 5, 6, 8, 2, 3];
+let bills = [234, 645, 234, 767, 234];
+
+getArraySum(nums);
+getArraySum(bills);
+*/
+
+/*
+function getProduct(num1, num2) {
+  let result = num1 * num2; // num1 = 10, num2 = undefined; 10 + undef
+  console.log(result);
+}
+
+getProduct(10);
+*/
+
+/*
+function getPow(num, pow = 1) {
+  let result = num ** pow;
+  console.log(result);
+}
+
+getPow(10);
+getPow(10, 5);
+getPow(32);
+*/
+
+/*
+function getPow(pow = 1, num) {
+  let result = num ** pow;
+  console.log(result);
+}
+
+getPow(10);
+*/
+
+/**
+ * возврат значения из функции
+ */
+/*
+function getPow(num, pow) {
+  let result = num ** pow;
+}
+
+getPow(10, 2);
+getPow(2, 2);
+
+console.log(result);*/
+
+/*
+function getPow(num, pow) {
+  let result = num ** pow;
+  return result;
+}
+
+let numPow = getPow(10, 2);
+console.log(numPow);
+console.log(numPow * 5);
+*/
+
+/*
+// посчитать сумму и произведение двух чисел
+function doMath(num1, num2) {
+  let sum = num1 + num2;
+  let prod = num1 * num2;
+
+  return [sum, prod];
+}
+
+let myMath = doMath(5, 5);
+console.log(myMath);
+*/
+
+/*
+// посчитать сумму и произведение двух чисел
+function doMath(num1, num2) {
+  let sum = num1 + num2;
+  let prod = num1 * num2;
+
+  // возвращаем ОБЪЕКТ
+  return { summa: sum, product: prod };
+}
+
+let myMath = doMath(5, 5);
+console.log(myMath);
+
+//let student = ["Иван", "Иванов", 22, 2, true];
+// объект
+let student = {
+  fName: "Иван",
+  lName: "Иванов",
+  age: 22,
+  cource: 2,
+  isAdmin: true,
+};
+*/
+
+/*
+// массив объектов
+let cart = [
+  {
+    title: "Пианино",
+    price: 321,
+    qty: 3,
+  },
+  {
+    title: "Дудка",
+    price: 123,
+    qty: 6,
+  },
+  {
+    title: "Флейта",
+    price: 654,
+    qty: 1,
+  },
+];
+console.log(cart);
+*/
+
+/*
+let student = {
+  fName: "Иван",
+  lName: "Иванов",
+  age: 22,
+  cource: 2,
+  isAdmin: true,
+};
+
+console.log(student);
+console.log(student.fName);
+console.log(student.lName);
+*/
